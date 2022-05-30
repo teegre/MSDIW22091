12c755a0ba94301f8969b4e01090b014d6827772
Stéphane MEYER <stm3y3r@icloud.com>
2022-05-30 09:00:14 +0200

    added licence

--- 9_symfony/records/public/js/script.js
+++ 9_symfony/records/public/js/script.js
@@ -1,4 +1,30 @@
+/*    
+  @licstart  The following is the entire license notice for the 
+  JavaScript code in this page.
+
+  Copyright (C) 2022  Stéphane MEYER (Teegre)
+
+  The JavaScript code in this page is free software: you can
+  redistribute it and/or modify it under the terms of the GNU
+  General Public License (GNU GPL) as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option)
+  any later version.  The code is distributed WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+
+  As additional permission under GNU GPL version 3 section 7, you
+  may distribute non-source (e.g., minimized or compacted) forms of
+  that code without the copy of the GNU GPL normally required by
+  section 4, provided you include this license notice and a URL
+  through which recipients can access the Corresponding Source.   
+
+
+  @licend  The above is the entire license notice
+  for the JavaScript code in this page.
+*/
+
 function updatePicture() {
+  /* update picture display */
   img = document.getElementsByTagName("img")[0];
   file = document.getElementById("form_record_picture").files[0];
   if (file.type.match('^image.*$')) {
@@ -10,7 +36,8 @@ function updatePicture() {
   }
 }
 
-function addSong(e) { 
+function addSong(e) {
+  /* add a text input to song collection */
   const collectionHolder = document.querySelector('.' + e.currentTarget.dataset.collectionHolderClass);
   const item = document.createElement('div');
   item.innerHTML = collectionHolder.dataset.prototype.replace(/__name__/g, collectionHolder.dataset.index);
@@ -21,6 +48,7 @@ function addSong(e) {
 }
 
 function removeSong(id) {
+  /* remove a text input from song collection */
   song = document.getElementById("song-" + id);
   song.parentNode.removeChild(song);
 }
